a43046078658001b9345e8509575765ab876b9e9
CatMuse <kairos.jiang@gmail.com>
2026-05-13 14:35:34 +0800

    Extract flashcard group modal

--- src/flashcard/components/controllers/index.ts
+++ src/flashcard/components/controllers/index.ts
@@ -1,5 +1,6 @@
 export { FlashcardOperations } from "./FlashcardOperations";
 export { FlashcardGroupManager } from "./FlashcardGroupManager";
+export * from "./FlashcardGroupModal";
 export { FlashcardProgressManager } from "./FlashcardProgress";
 export * from "./FlashcardReviewQueue";
 export { FlashcardUtils } from "./FlashcardUtils";
